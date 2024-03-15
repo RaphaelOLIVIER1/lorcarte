6461f01afbfc721b57871dcc68ee21784abf54bf
Raphael Oivier <olivierraphael688@gmail.com>
2024-03-15 18:44:25 +0100

    mise en place de header et footer et librairie

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lorcarte</title>
-</head>
+<?php
+include_once 'header.php';
+?>
+
 <body>
-    <h1>Lorcarte</h1>
+    <div>
+        <h1>Lorcarte</h1>
+    </div>
 </body>
-</html>
+
+<?php
+include_once 'footer.php';
+?>
---
+++ javascript/initialisation_aos.js
@@ -0,0 +1,6 @@
+ // use a script tag or an external JS file
+ document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(Flip,ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin,RoughEase,ExpoScaleEase,SlowMo,CustomEase)
+  // gsap code here!
+ });
+
